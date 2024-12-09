8793cfdba5e753aee69338b2f2315bdf0fa7029e
utentica <rodrigogmrpreto@gmail.com>
2024-12-09 14:59:52 +0000

    leveldone

--- systems/difficulty_menu.gd
+++ systems/difficulty_menu.gd
@@ -13,3 +13,15 @@ func _process(delta: float) -> void:
 
 func _on_return_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://systems/main_menu.tscn")
+
+
+func _on_level_1_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+
+
+func _on_level_2_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/level_2.tscn")
+
+
+func _on_level_3_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/level_3.tscn")
